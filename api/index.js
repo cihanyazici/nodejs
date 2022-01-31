136270b31d58fd6d47d7ce25c7c775390b546811
Cihan YAZICI <cyazici@gmail.com>
2022-01-31 23:33:31 +0300

    imdb

--- api/index.js
+++ api/index.js
@@ -1,17 +1,5 @@
-const Genius = require("genius-lyrics") // import the library
-const Client = new Genius.Client() // setup the Genius Lyrics API
 const IMDb = require('imdb-light');
 
-async function retrieveLyrics() {
-  const songs = await Client.songs.search("Pop Smoke") // Get songs from Pop Smoke
-
-  const indexSongs = Math.floor(Math.random() * Math.floor(songs.length))
-  const lyrics = await songs[indexSongs].lyrics() // Get random song
-
-  const arrLyrics = lyrics.split("\n").filter(l => l.length && l[0] !== '[')
-  const indexLyrics = Math.floor(Math.random() * Math.floor(arrLyrics.length))
-  return arrLyrics[indexLyrics] // Return random lyrics
-}
 
 function fetch(id) {
   return new Promise(function (resolve, reject) {
@@ -26,12 +14,11 @@ async function test(id) {
   return quote;
 }
 
-module.exports = async (req, res) => { // this function will be launched when the API is called.ü
-  const { id } = req.query;
-
+module.exports = async (req, res) => { 
+  movie=await test(req.query.id)
   try {
-    res.send(await test(id)) // send the lyrics
+    res.send({rating:movie.Rating}) 
   } catch (err) {
-    res.send(err) // send the thrown error
+    res.send(err) 
   }
 }
