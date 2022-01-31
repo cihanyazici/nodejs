0e7656885b109b2fb295737ea080458abb46fba6
Cihan YAZICI <cyazici@gmail.com>
2022-01-31 14:01:23 +0300

    imdb

---
+++ api/imdb.js
@@ -0,0 +1,24 @@
+const IMDb = require('imdb-light');
+
+
+function fetch(id) {
+  return new Promise(function (resolve, reject) {
+          IMDb.fetch(id, (details) => {
+                  resolve(details);
+          });
+  });
+}
+
+async function test(id) {
+  var quote = await fetch(id);
+  return quote;
+}
+
+module.exports = async (req, res) => { 
+  movie=await test(req.query.id)
+  try {
+    res.send({rating:movie.Rating}) 
+  } catch (err) {
+    res.send(err) 
+  }
+}
